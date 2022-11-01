45942d956dde499ae57964d94514eb45d6332344
Henrique Tavares <ihenrits@gmail.com>
2022-11-01 19:40:40 -0300

    add eslint and prettier

---
+++ web/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'node': true,
+		'es2020': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'react/react-in-jsx-scope': 'off',
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
