2533cf41fa5bd7cc597bba6cb786b9f39e8db88a
Henrique Tavares <ihenrits@gmail.com>
2022-10-31 22:29:59 -0300

    setup fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,10 @@ import Fastify from 'fastify';
 async function start() {
   const fastify = Fastify({ logger: true });
 
+  fastify.get('/pools/count', async (request, reply) => {
+    return { count: 15 };
+  });
+
   await fastify.listen({ port: 3333 });
 }
 
