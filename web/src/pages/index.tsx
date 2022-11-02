824fa29ceb7bc6a6b02c3266cd904876a92c9b34
Henrique Tavares <ihenrits@gmail.com>
2022-11-02 18:15:08 -0300

    create pool

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import Image from 'next/image';
 
 import { api } from '../lib/axios';
@@ -9,6 +10,36 @@ interface IHomeProps {
 }
 
 export default function Home({ poolCount, guessCount, userCount }: IHomeProps) {
+  const [poolTitle, setPoolTitle] = useState('');
+
+  function handlePoolTitleChange(event: ChangeEvent<HTMLInputElement>) {
+    const target = event.target;
+
+    setPoolTitle(target.value);
+  }
+
+  async function handleCreatePool(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+
+      const { code } = response.data;
+
+      await navigator.clipboard.writeText(code);
+
+      alert('Bolão criado com sucesso, o código foi copiado para a área de transferência.');
+
+      setPoolTitle('');
+    } catch (error) {
+      console.error(error);
+
+      alert('Fala ao criar o bolão, tente novamente!');
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
@@ -32,12 +63,14 @@ export default function Home({ poolCount, guessCount, userCount }: IHomeProps) {
           </strong>
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={handleCreatePool} className="mt-10 flex gap-2">
           <input
             type="text"
             required
             placeholder="Qual nome do seu bolão?"
-            className="flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm text-gray-100"
+            onChange={handlePoolTitleChange}
+            value={poolTitle}
           />
 
           <button
