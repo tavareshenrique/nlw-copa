4cf3d976f3f3330242aada337636c8fb0877c127
Henrique Tavares <ihenrits@gmail.com>
2022-11-02 16:48:43 -0300

    new routs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
+
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
@@ -14,10 +17,45 @@ async function start() {
     origin: true
   });
 
-  fastify.get('/pools/count', async (request, reply) => {
+  fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string()
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({
+      length: 6
+    });
+
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    });
+
+    return await reply.status(201).send({ title, code });
   });
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' });
