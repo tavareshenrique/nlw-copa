a41d281e1057cab150feddf39098ea6617f6c73b
Henrique Tavares <ihenrits@gmail.com>
2022-11-02 17:56:16 -0300

    styling app

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="bg-gray-900">
+      <body className="bg-gray-900 bg-app bg-no-repeat bg-cover">
         <Main />
         <NextScript />
       </body>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
         <Image src="/assets/logo.svg" alt="NLW COPA" width={213} height={41} quality={100} />
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
 
-        <div>
+        <div className="mt-10 flex items-center gap-2">
           <Image
             src="/assets/users-avatar-example.png"
             alt="Imagem de algumas pessoas que já usam o NLW Copa"
@@ -17,31 +19,48 @@ export default function Home() {
             quality={100}
           />
 
-          <strong>
-            <span>+12.592</span> pessoas já estão usando.
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão usando.
           </strong>
         </div>
 
-        <form>
-          <input type="text" required placeholder="Qual nome do seu bolão?" />
+        <form className="mt-10 flex gap-2">
+          <input
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm"
+          />
 
-          <button type="submit">Criar meu bolão</button>
+          <button
+            type="submit"
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+          >
+            Criar meu bolão
+          </button>
         </form>
 
-        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
 
-        <div>
-          <Image src="/assets/icon-check.svg" alt="Icone de check" width={40} height={40} quality={100} />
-          <div>
-            <span>+2.034</span>
-            <span>Bolões criados</span>
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
+            <Image src="/assets/icon-check.svg" alt="Icone de check" width={40} height={40} quality={100} />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Bolões criados</span>
+            </div>
           </div>
-        </div>
-        <div>
-          <Image src="/assets/icon-check.svg" alt="Icone de check" width={40} height={40} quality={100} />
-          <div>
-            <span>+192.847</span>
-            <span>Palpites enviados</span>
+
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-center gap-6">
+            <Image src="/assets/icon-check.svg" alt="Icone de check" width={40} height={40} quality={100} />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+192.847</span>
+              <span>Palpites enviados</span>
+            </div>
           </div>
         </div>
       </main>
