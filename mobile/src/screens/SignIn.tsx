d6538c09054897bc6859a74e428ddebace5abb04
Henrique Tavares <ihenrits@gmail.com>
2022-11-03 20:10:14 -0300

    add svg in mobile

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,11 @@
-import { Center, Text } from 'native-base';
+import { Center } from 'native-base';
+
+import Logo from '../assets/logo.svg';
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        Pitica ♥️
-      </Text>
+      <Logo width={212} height={40} />
     </Center>
   );
 }
