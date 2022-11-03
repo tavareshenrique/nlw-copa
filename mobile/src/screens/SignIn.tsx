61a9f11ae61815d22c206a2496c3d58a33596fb4
Henrique Tavares <ihenrits@gmail.com>
2022-11-03 20:45:25 -0300

    finishing SignIn screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Icon } from 'native-base';
+import { Center, Icon, Text } from 'native-base';
 import { Fontisto } from '@expo/vector-icons';
 
 import Button from '../components/Button';
@@ -7,14 +7,20 @@ import Logo from '../assets/logo.svg';
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
         type="SECONDARY"
         title="ENTRAR COM GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" size="md" />}
+        mt={12}
       />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilize nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   );
 }
