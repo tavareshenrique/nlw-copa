a330d52ca350f294a224e3659d5647182e2431ea
Henrique Tavares <ihenrits@gmail.com>
2022-11-05 13:53:21 -0300

    list games in a pool route

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,5 +1,49 @@
 import { FastifyInstance } from 'fastify';
 
-// import { prisma } from '../lib/prisma';
+import { z } from 'zod';
 
-export async function gameRoutes(fastify: FastifyInstance) {}
+import { authenticate } from '../plugins/authenticate';
+
+import { prisma } from '../lib/prisma';
+
+export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    '/pools/:id/games',
+    {
+      onRequest: [authenticate]
+    },
+    async (request) => {
+      const getPoolParams = z.object({
+        id: z.string()
+      });
+
+      const { id } = getPoolParams.parse(request.params);
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: 'desc'
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                poolId: id
+              }
+            }
+          }
+        }
+      });
+
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined
+          };
+        })
+      };
+    }
+  );
+}
