12400fdc04fd4002c0ed2f102610d586f6f1fe3a
Henrique Tavares <ihenrits@gmail.com>
2022-11-05 14:09:12 -0300

    create a guess route

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -46,4 +46,87 @@ export async function gameRoutes(fastify: FastifyInstance) {
       };
     }
   );
+
+  fastify.post(
+    '/pools/:poolId/games/:gameId/guessess',
+    {
+      onRequest: [authenticate]
+    },
+    async (request, reply) => {
+      const createGuessParams = z.object({
+        poolId: z.string(),
+        gameId: z.string()
+      });
+
+      const createGuessBody = z.object({
+        firstTeamScore: z.number(),
+        secondTeamScore: z.number()
+      });
+
+      const { poolId, gameId } = createGuessParams.parse(request.params);
+
+      const { firstTeamScore, secondTeamScore } = createGuessBody.parse(
+        request.body
+      );
+
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_poolId: {
+            poolId,
+            userId: request.user.sub
+          }
+        }
+      });
+
+      if (!participant) {
+        return await reply.status(403).send({
+          message: "You're not allowed to create a guess inside this pool."
+        });
+      }
+
+      const guess = await prisma.guess.findUnique({
+        where: {
+          participantId_gameId: {
+            participantId: participant.id,
+            gameId
+          }
+        }
+      });
+
+      if (guess) {
+        return await reply.status(409).send({
+          message: "You're already sent a guess to this game on this pool."
+        });
+      }
+
+      const game = await prisma.game.findUnique({
+        where: {
+          id: gameId
+        }
+      });
+
+      if (!game) {
+        return await reply.status(404).send({
+          message: 'Game not found.'
+        });
+      }
+
+      if (game.date < new Date()) {
+        return await reply.status(409).send({
+          message: 'This game already happened.'
+        });
+      }
+
+      await prisma.guess.create({
+        data: {
+          gameId,
+          participantId: participant.id,
+          firstTeamScore,
+          secondTeamScore
+        }
+      });
+
+      return await reply.status(201).send();
+    }
+  );
 }
