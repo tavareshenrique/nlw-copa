34da6acb8fa9382bb2b8304e676818b422ef19b5
Henrique Tavares <ihenrits@gmail.com>
2022-11-05 19:13:27 -0300

    join in a pool

--- mobile/src/screens/Find.tsx
+++ mobile/src/screens/Find.tsx
@@ -1,8 +1,69 @@
-import { Heading, VStack } from 'native-base';
+import { useState } from 'react';
+import { Heading, useToast, VStack } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+
+import { api } from '../services/api';
 
 import { Button, Header, Input } from '../components';
 
 export function Find() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = useState('');
+
+  const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function handleJoinPool() {
+    try {
+      setIsLoading(true);
+
+      if (!code.trim()) {
+        return toast.show({
+          title: 'Digite o código do bolão.',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      await api.post('/pools/join', { code });
+
+      toast.show({
+        title: 'Você entrou no bolão com sucesso.',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      navigate('pools');
+    } catch (error) {
+      console.log(error);
+
+      setIsLoading(false);
+
+      if (error.response.status === 404) {
+        return toast.show({
+          title: 'Não foi possível encontrar o bolão.',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      if (error.response.status === 409) {
+        return toast.show({
+          title: 'Você já está nesse bolão.',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      toast.show({
+        title:
+          'Houve algum problema ao tentar entrar no bolão, tente novamente mais tarde.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Buscar por código" showBackButton />
@@ -18,9 +79,19 @@ export function Find() {
           Encontre um bolão através de {'\n'} seu código único.
         </Heading>
 
-        <Input mb={2} placeholder="Qual o código do bolão?" />
+        <Input
+          mb={2}
+          placeholder="Qual o código do bolão?"
+          autoCapitalize="characters"
+          onChangeText={setCode}
+          value={code}
+        />
 
-        <Button title="BUSCAR BOLÃO" />
+        <Button
+          title="BUSCAR BOLÃO"
+          onPress={handleJoinPool}
+          isLoading={isLoading}
+        />
       </VStack>
     </VStack>
   );
