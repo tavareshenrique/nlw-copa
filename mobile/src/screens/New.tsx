73d570c7efacfbe31b93f12be657615ec3780521
Henrique Tavares <ihenrits@gmail.com>
2022-11-05 18:15:32 -0300

    create new pool

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,10 +1,55 @@
-import { Heading, Text, VStack } from 'native-base';
+import { useState } from 'react';
+
+import { Heading, Text, VStack, useToast } from 'native-base';
+
+import { api } from '../services/api';
 
 import { Button, Header, Input } from '../components';
 
 import Logo from '../assets/logo.svg';
 
 export function New() {
+  const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast();
+
+  async function handlePoolCreate() {
+    if (!title.trim()) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+
+    try {
+      setIsLoading(true);
+
+      await api.post('/pools', {
+        title,
+      });
+
+      toast.show({
+        title: 'Bolão criado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      setTitle('');
+    } catch (error) {
+      console.log(error);
+
+      return toast.show({
+        title: 'Não foi possível criar o bolão.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -22,9 +67,18 @@ export function New() {
           Crei seu próprio bolão da copa {'\n'} e compartilhe entre amigos!
         </Heading>
 
-        <Input mb={2} placeholder="Qual o nome do seu bolão?" />
+        <Input
+          mb={2}
+          placeholder="Qual o nome do seu bolão?"
+          onChangeText={setTitle}
+          value={title}
+        />
 
-        <Button title="CRIAR MEU BOLÃO" />
+        <Button
+          title="CRIAR MEU BOLÃO"
+          onPress={handlePoolCreate}
+          isLoading={isLoading}
+        />
 
         <Text color="gray.300" fontSize="sm" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único que poderá usar
